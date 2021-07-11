a1fbb3eb94c0e513e0233dc7d15e77be135d35d4
Bartlomiej Stepien <masafion@gmail.com>
2021-07-11 14:05:38 +0200

    6.2. Klasa Calculator i jej test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calcTest = new Calculator();
+
+        //add method test
+        if (calcTest.add(11,22)==33) {
+            System.out.println("test OK");
+        }
+        else{
+            System.out.println("Error!");
+        }
+
+        //substract method test
+        if (calcTest.substract(100,99)==1) {
+            System.out.println("test OK");
+        }
+        else{
+            System.out.println("Error!");
+        }
+
     }
 }
