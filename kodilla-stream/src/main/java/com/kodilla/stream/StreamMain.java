da5a1e81afeff1869249be57be8927afca6f5999
Bartlomiej Stepien <masafion@gmail.com>
2021-07-16 18:52:03 +0200

    7.2 Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,20 +1,24 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String textToBeautify = "Zwykły tekst";
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        String textToBeautify = "Zwykły tekst";
+//
+//        System.out.println("1: "+poemBeautifier.beautify("Czy upiększacz działa?", n -> ">>> " + n + " <<<"));
+//        System.out.println("2: "+poemBeautifier.beautify(textToBeautify, n -> n.replaceAll("Zwykły", "Niezwykły")));
+//        System.out.println("3: "+poemBeautifier.beautify("tylko duże litery", n -> n.toUpperCase()));
+//        System.out.println("4: "+poemBeautifier.beautify(textToBeautify, n -> n.substring(8)));
+//        System.out.println("5: "+poemBeautifier.beautify(textToBeautify, n -> n.substring(0, 7)));
+//
+//        //wynik ostatniego printa u mnie jest taki: "zwyk�y tekst", zamiast "ł" wyświetla "�"
+//        System.out.println("6: "+poemBeautifier.beautify(textToBeautify, n -> n.toLowerCase()));
 
-        System.out.println("1: "+poemBeautifier.beautify("Czy upiększacz działa?", n -> ">>> " + n + " <<<"));
-        System.out.println("2: "+poemBeautifier.beautify(textToBeautify, n -> n.replaceAll("Zwykły", "Niezwykły")));
-        System.out.println("3: "+poemBeautifier.beautify("tylko duże litery", n -> n.toUpperCase()));
-        System.out.println("4: "+poemBeautifier.beautify(textToBeautify, n -> n.substring(8)));
-        System.out.println("5: "+poemBeautifier.beautify(textToBeautify, n -> n.substring(0, 7)));
-
-        //wynik ostatniego printa u mnie jest taki: "zwyk�y tekst", zamiast "ł" wyświetla "�"
-        System.out.println("6: "+poemBeautifier.beautify(textToBeautify, n -> n.toLowerCase()));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
